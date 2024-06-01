f270b07548fff5b6d89d45612b8c278a9925aaa9
nol4lej <nome.ahumada@outlook.com>
2024-06-01 17:45:05 -0400

    DATABASE_URL added to schema

--- products-ms/src/config/envs.ts
+++ products-ms/src/config/envs.ts
@@ -3,10 +3,12 @@ import * as joi from 'joi';
 
 interface EnvVars {
     PORT: number;
+    DATABASE_URL: string;
 }
 
 const envsSchema = joi.object({
-    PORT: joi.number().required()
+    PORT: joi.number().required(),
+    DATABASE_URL: joi.string().required(),
 })
 .unknown(true);
 
@@ -21,4 +23,5 @@ export const envVars = value;
 
 export const envs = {
     port: envVars.PORT,
+    datebaseUrl: envVars.DATABASE_URL,
 }
