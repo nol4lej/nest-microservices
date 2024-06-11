267b1b6d98a4df98e67743bdc2dac87887780874
nol4lej <nome.ahumada@outlook.com>
2024-06-11 19:27:22 -0400

    remove product added / update product added / findOne modified: find just availabled products - Changed NotFoundException to RpcException (handled by the gateway), using an HTTP status response with a custom message

--- products-ms/src/products/products.service.ts
+++ products-ms/src/products/products.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from 'src/common';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -24,13 +25,18 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
 
     const { page, limit } = paginationDto;
 
-    const totalPages = await this.product.count();
+    const totalPages = await this.product.count({
+      where: { available: true }
+    });
     const lastPage = Math.ceil( totalPages / limit)
 
     return {
       data: await this.product.findMany({
         skip: ( page - 1 ) * limit,
-        take: limit
+        take: limit,
+        where: {
+          available: true
+        }
       }),
       meta: { // envio metadata 
         totalPages: totalPages,
@@ -42,21 +48,44 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
 
   async findOne(id: number) {
     const product = await this.product.findFirst({
-      where: { id }
+      where: { 
+        id,
+        available: true 
+      }
     })
 
     if ( !product ){
-      throw new NotFoundException(`Product id #${id} not found.`)
+      throw new RpcException({
+        message: `Product id #${id} not found.`,
+        status: HttpStatus.BAD_REQUEST
+      })
     }
 
     return product
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateProductDto: UpdateProductDto) {
+
+    const { id: __, ...data} = updateProductDto;
+
+    await this.findOne(id);
+
+    return this.product.update({
+      where: { id },
+      data: data
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+
+    await this.findOne(id);
+
+    return this.product.update({
+      where: { id },
+      data: {
+        available: false
+      }
+    })
+
   }
 }
