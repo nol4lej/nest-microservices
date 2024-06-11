661b14f01388c79771be2f35d225cccaf1b3621b
nol4lej <nome.ahumada@outlook.com>
2024-06-11 19:18:02 -0400

    Service modified from REST to microservice using 'createMicroservice' from @nestjs/microservices // Service context options configured with TCP transport and port for communication with the gateway

--- products-ms/src/main.ts
+++ products-ms/src/main.ts
@@ -2,21 +2,32 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config/envs';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
 
-  const logger = new Logger('Main')
+  const logger = new Logger('Main');
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port
+      }
+    }
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true
     })
-  )
+  );
 
-  await app.listen( envs.port );
-  logger.log(`Running on port ${envs.port}`);
+  await app.listen();
+  logger.log(`Products Microservices running on port ${envs.port}`);
+
 }
+
 bootstrap();
